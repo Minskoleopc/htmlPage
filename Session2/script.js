1949ac7ab5218b3fb3263a2c96681283f07b7388
Minskoleopc <minskoleopc@gmail.com>
2023-03-10 08:26:11 +0530

    added

---
+++ Session2/script.js
@@ -0,0 +1,43 @@
+// html css javascript
+// html - text , css - style , useraction -- javascript
+// <h1 id = "one" class = "two" name = "nm">chinmay</h1>
+
+// css selector 
+// tagName 
+let q11 = document.querySelector('h1')
+console.log(q11)
+
+// // className 
+// let q12 = document.querySelector('.two')
+// console.log(q12)
+
+// // id 
+// let q13 = document.querySelector('#one')
+// console.log(q13)
+
+// // attribute
+// // tagName[attribute = "value"]
+// let q14 =document.querySelector('h1[name= "nm"]')
+// console.log(q14)
+
+q11.addEventListener('mouseover',function(){
+    q11.style.color = "red"; 
+    q11.style.backgroundColor = "yellow";
+    q11.textContent = "Sarika"
+})
+
+
+// Java ------ selenium scripting ----- automation (webapplication)
+
+
+
+// HTML   CSS   Javascript
+
+
+//Design webpage ----elements
+// Css - cascading style sheet
+// Javascript - dynamic
+
+
+// Css selectors 
+// Xpath 
